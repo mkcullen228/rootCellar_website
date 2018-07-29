30bed2131f87b214127796d1e3162cbde369be2b
mkcullen228 <maura.cullen28@gmail.com>
2018-07-29 15:00:18 -0700

    Addding updates

--- app/user_profile_support/rootseller/visualizations.py
+++ app/user_profile_support/rootseller/visualizations.py
@@ -2,8 +2,10 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 import warnings
 warnings.filterwarnings('ignore')
 warnings.filterwarnings("ignore", category=DeprecationWarning)
